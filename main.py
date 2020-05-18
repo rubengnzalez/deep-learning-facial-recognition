87c3c7508647d50724a9200d689a00433d359de6
rubengnzalez <ruben.gonzalez.lozano@outlook.com>
2020-05-18 21:27:48 +0200

    Split into Train/Test datasets is implemented and it now is stored in disk according to the folders structure preferred by Keras ImageDataGenerator.flow...

--- main.py
+++ main.py
@@ -4,14 +4,16 @@ __author__ = 'Ruben Gonzalez Lozano'
 __email__ = '100284010@alumnos.uc3m.es'
 
 from common.utils import load_config, init_logger
-from preprocess import prepare_data_in_folders
+import os
+import preprocess as pr
 
 if __name__ == '__main__':
     cfg = load_config('./conf/conf.yaml')
-    logger = init_logger(cfg['logging'], cfg['logging']['name'])
-    prepare_data_in_folders(
-        cfg['preprocess']['prepare']['input']['path'],
-        cfg['preprocess']['prepare']['input']['file_name_format'],
-        cfg['preprocess']['prepare']['output']['criteria'],
-        cfg['preprocess']['prepare']['output']['path'],
-        cfg['preprocess']['prepare']['output']['classes'])
+    init_logger(cfg['logging'], cfg['logging']['name'])
+    x, y = pr.load_unsorted_data(raw_path, criteria, file_name_format, classes, classes_ranges)
+    train_x, test_x, train_y, test_y = pr.get_train_test_split(x, y, test_size=0.2, random_state=42, shuffle=True, stratify=y)
+    target_path = '/home/ruben/workspace/tfg/deep-learning-facial-recognition/data'
+    dest_path = os.path.join(target_path, criteria, 'training')
+    pr.prepare_data_in_folders(train_x, train_y, dest_path, classes)
+    dest_path = os.path.join(target_path, criteria, 'test')
+    pr.prepare_data_in_folders(test_x, test_y, dest_path, classes)
--- preprocess.py
+++ preprocess.py
@@ -5,70 +5,118 @@ __email__ = '100284010@alumnos.uc3m.es'
 
 import logging
 import os
-
+from sklearn.model_selection import train_test_split
 from common.exceptions import PreprocessingException
 from common.utils import remove_dir, create_dir, copy_file, load_config, \
     init_logger
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 
-def prepare_data_in_folders(input_path, file_name_format, criteria,
-                            output_path, classes, logger_name='log'):
+def get_class_id(label, classes):
     """
-# TODO:  ############################################################################
-    :param input_path:
-    :param file_name_format:
-    :param criteria:
-    :param output_path:
+    TODO:  ############################################################################
+    :param label:
     :param classes:
-    :param logger_name:
     :return:
     """
-    log = logging.getLogger(logger_name)
-    log.info(
-        'Classifying images into their corresponding folders according to '
-        'criteria "{crit}" and the following classes distribution: {clas}'
-        ''. format(crit=criteria, clas=classes))
-    full_input_path = os.path.abspath(input_path)
-    full_output_path = os.path.abspath(output_path)
-    if os.path.exists(full_input_path) and \
-            os.path.exists(full_output_path):
-        crit_path = os.path.join(full_output_path, criteria)
-        crit_index = file_name_format.split('_').index(criteria)
-        if os.path.exists(crit_path):
-            # just in case it exists and contains previous executions data
-            remove_dir(crit_path)
-        else:
-            create_dir(crit_path)
-        for r, d, f in os.walk(full_input_path):
-            for file in f:
-                if os.path.isfile(os.path.join(r, file)):
-                    crit_value = file.split('_')[crit_index]
-                    ext = '.' + file.split('.')[-1]
-                    for k, v in classes.items():
-                        if v[0] <= int(crit_value) <= v[1]:
-                            if not os.path.exists(
-                                    os.path.join(crit_path, str(k))):
-                                create_dir(os.path.join(crit_path, str(k)))
-                            copy_file(os.path.join(r, file),
-                                      os.path.join(
-                                          crit_path,
-                                          str(k),
-                                          file.split('.')[0] + ext))
-        log.info('Data prepared in their corresponding directories under main '
-                 'path {path} successfully'.format(path=crit_path))
+    return classes.index(label)
+
+
+def get_class_name(id, classes):
+    """
+    TODO:  ############################################################################
+    :param id:
+    :param classes:
+    :return:
+    """
+    return classes[id if isinstance(id, int) else int(id)]
+
+
+def assign_class(real_value, classes_ranges):
+    """
+    TODO:  ############################################################################
+    :param real_value:
+    :param classes_ranges:
+    :return:
+    """
+    val = real_value if isinstance(id, int) else int(real_value)
+    for k in classes_ranges.keys():
+        if val in range(classes_ranges[k][0], classes_ranges[k][1]+1):
+            # print('{} belongs to class "{}"'.format(str(real_value), k))
+            return k
+
+
+def load_unsorted_data(data_path, criteria, file_name_format, classes,
+                       classes_ranges):
+    """
+    TODO:  ############################################################################
+    :param data_path:
+    :param criteria:
+    :param file_name_format:
+    :param classes:
+    :param classes_ranges:
+    :return:
+    """
+    lab_idx = file_name_format.split('_').index(criteria)
+    lab, path = [], []
+    files = [os.path.join(data_path, f) for f in os.listdir(data_path)
+             if os.path.isfile(os.path.join(data_path, f))
+             and f.endswith('.jpg')]
+    for f in files:
+        path.append(f)
+        f_name = f.split('/')[-1]
+        lab.append(get_class_id(
+            assign_class(
+                int(f_name.split('_')[lab_idx]), classes_ranges), classes))
+    return path, lab
+
+
+def get_train_test_split(x, y, test_size, random_state=None, shuffle=True,
+                         stratify=None):
+    """
+    TODO:  ############################################################################
+    :param x:
+    :param y:
+    :param test_size:
+    :param random_state:
+    :param shuffle:
+    :param stratify:
+    :return:
+    """
+
+    return train_test_split(x,
+                            y,
+                            test_size=test_size,
+                            random_state=random_state,
+                            shuffle=shuffle,
+                            stratify=stratify)
+
+
+def prepare_data_in_folders(x, y, target_path, classes):
+    """
+    TODO: ################################################################################
+    :param x:
+    :param y:
+    :param target_path:
+    :param classes:
+    :return:
+    """
+    full_path = os.path.abspath(target_path)
+    if os.path.exists(full_path):
+        # just in case it exists and contains previous executions data
+        remove_dir(full_path)
     else:
-        raise PreprocessingException(
-            'An error occurred while classifying images in their '
-            'corresponding folders')
+        create_dir(full_path)
+    i = 0
+    for f in x:
+        fname = f.split('/')[-1]
+        cls = get_class_name(y[i], classes)
+        dst = os.path.join(full_path, cls)
+        if not os.path.exists(dst):
+            create_dir(dst)
+        dst = os.path.join(dst, fname)
+        copy_file(f, dst)
+        i += 1
 
 
 if __name__ == '__main__':
-    cfg = load_config('./conf/conf.yaml')
-    init_logger(cfg['logging'], cfg['logging']['name'])
-    prepare_data_in_folders(
-        cfg['preprocess']['prepare']['input']['path'],
-        cfg['preprocess']['prepare']['input']['file_name_format'],
-        cfg['preprocess']['prepare']['output']['criteria'],
-        cfg['preprocess']['prepare']['output']['path'],
-        cfg['preprocess']['prepare']['output']['classes'])
+    pass
