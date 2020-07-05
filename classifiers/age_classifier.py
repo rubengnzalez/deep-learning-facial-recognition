ef5b08b51d9a07bf0443a796db8f6c11984a2b19
rubengnzalez <ruben.gonzalez.lozano@outlook.com>
2020-07-05 20:47:21 +0200

    more changes on AgeClassifier

--- classifiers/age_classifier.py
+++ classifiers/age_classifier.py
@@ -7,10 +7,14 @@ import logging
 import os
 from datetime import datetime
 import tensorflow as tf
+from sklearn.metrics import confusion_matrix, classification_report
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPool2D, Dropout, Flatten, Dense
 import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sn
+import pandas as pd
 from tensorflow.python.keras.models import load_model
 from tensorflow.python.keras.utils.vis_utils import plot_model
 from common.utils import create_dir
@@ -31,14 +35,15 @@ class AgeClassifier:
             self.generate_datasets(training_path, test_path)
         self.model, self.history = None, None
         self.fig_path = fig_path
+        self.name = 'BLABLABLA'
 
     @staticmethod
     def generate_datasets(training_path, test_path):
         """
-        # TODO: ###################################################################
-        :param training_path:
-        :param test_path:
-        :return:
+        It creates Data Generators for data augmentation.
+        :param training_path: Path to training set
+        :param test_path: Path to test set
+        :return: Tuple of generators: (training, test)
         """
         training_datagen = ImageDataGenerator(rescale=1. / 255,
                                               rotation_range=0.3,
@@ -57,20 +62,20 @@ class AgeClassifier:
                                                     target_size=(64, 64),
                                                     batch_size=32,
                                                     class_mode='categorical',
-                                                    shuffle=True,
+                                                    shuffle=False,
                                                     seed=42)
         return training_set, test_set
 
     def show_summary(self):
         """
-        # TODO: ###################################################################
+        It shows a summary of the model architecture
         """
         self.logger.info(self.model.summary())
 
     def compile(self, summary=True):
         """
-        # TODO: ###################################################################
-        :param summary:
+        It builds and compiles the deep learning network
+        :param summary: Determines whether the model summary is shown or not
         """
         # Build architecture
         self.model = Sequential()
@@ -78,12 +83,12 @@ class AgeClassifier:
             Conv2D(filters=32, kernel_size=(3, 3), input_shape=(64, 64, 3),
                    activation='relu'))
         self.model.add(MaxPool2D(pool_size=(2, 2)))
-        self.model.add(
-            Conv2D(filters=32, kernel_size=(3, 3), activation='relu'))
-        self.model.add(MaxPool2D(pool_size=(2, 2)))
-        self.model.add(
-            Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
-        self.model.add(MaxPool2D(pool_size=(2, 2)))
+        # self.model.add(
+        #     Conv2D(filters=32, kernel_size=(3, 3), activation='relu'))
+        # self.model.add(MaxPool2D(pool_size=(2, 2)))
+        # self.model.add(
+        #     Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+        # self.model.add(MaxPool2D(pool_size=(2, 2)))
         self.model.add(Flatten())
         self.model.add(Dense(units=128, activation='relu'))
         self.model.add(Dropout(0.3))
@@ -99,21 +104,64 @@ class AgeClassifier:
 
     def train(self):
         """
-        # TODO: ####################################################################
+        It trains the model i.e. it calls the fit() method from model to train
+        the network according to the training set generated.
         """
+        training_steps = (18966 // 32)
+        test_steps = (4742 // 32)
         self.history = self.model.fit(self.training_set,
-                                      steps_per_epoch=(18966 // 32),
+                                      steps_per_epoch=training_steps,
                                       epochs=25,
                                       validation_data=self.test_set,
-                                      validation_steps=(4742 // 32))
+                                      validation_steps=test_steps)
+
+    def plot_confusion_matrix(self, steps, target_names,
+                              set_name='', save_path=None):
+        """
+        It plots confusion matrix and classification report.
+        :param steps: steps for prediction
+        :param target_names: list of classes
+        :param set_name: Name of the set to be displayed
+        :param save_path: Path to save the figure
+        """
+        if not save_path:
+            save_path = self.fig_path
+        y = self.model.predict_generator(self.test_set, steps=steps)
+        y_pred = np.argmax(y, axis=1)
+        self.logger.info('Confusion Matrix')
+        conf_mat = confusion_matrix(self.test_set.classes, y_pred)
+        self.logger.info(conf_mat)
+        self.logger.info('Classification Report')
+        clas_rep = classification_report(self.test_set.classes,
+                                         y_pred,
+                                         target_names=target_names)
+        self.logger.info(clas_rep)
+        con_mat_norm = np.around(
+            conf_mat.astype('float') / conf_mat.sum(axis=1)[:, np.newaxis],
+            decimals=2)
+        con_mat_df = pd.DataFrame(con_mat_norm,
+                                  index=self.test_set.classes,
+                                  columns=self.test_set.classes)
+        fig, axs = plt.subplots()
+        fig.set_size_inches(8, 8)
+        sn.heatmap(con_mat_df, annot=True, cmap=plt.cm.Blues)
+        fig.tight_layout()
+        axs.set_ylabel('True')
+        axs.set_xlabel('Predicted')
+        fig.show()
+        if save_path:
+            fname = '{}_confusion_matrix'.format(self.name) if not set_name \
+                else '{}_confusion_matrix'.format(set_name)
+            self.save_figure(fig, fname, save_path)
 
     def plot_model(self, file_path, show_shapes=True, show_layer_names=True):
         """
-        # TODO: #####################################################################
-        :param file_path:
-        :param show_shapes:
-        :param show_layer_names:
-        :return:
+        It creates a file with a plot of the model/network architecture. It
+        shows the shapes and the layers if wanted.
+        :param file_path: path where the file will be created
+        :param show_shapes: boolean that determines if shapes should be shown
+        :param show_layer_names: boolean that determines if layer names should
+        be shown
         """
         plot_model(self.model,
                    to_file=file_path,
@@ -144,7 +192,7 @@ class AgeClassifier:
 
     def show_training_history(self, set_name='', save_path=None):
         """
-        # TODO: ###################################################################
+        Plot training history based on accuracy and loss
         :param set_name: Name of the set to be displayed
         :param save_path: Path to save the figure
         :return:
@@ -173,28 +221,27 @@ class AgeClassifier:
         axs[1].legend(['train', 'test'], loc='upper left')
         fig.show()
         if save_path:
-            fname = 'training_history' if not set_name \
+            fname = '{}_training_history'.format(self.name) if not set_name \
                 else '{}_training_history'.format(set_name)
             self.save_figure(fig, fname, save_path)
 
-    # TODO: SAVE MODEL, WEIGHTS AND HISTORY (DICT, with pickle) ON DISK FOR LATER WORKING SESSIONS
-    def save_model(self, path, name):
+    def save_model(self, path):
         """
-        # TODO: ####################################################################
-        :param path:
-        :param name:
+        It saves the model on disk as well as the weights assigned to the
+        neurons in the network
+        :param path: path where the model and weight files should be stored
         """
         if not os.path.exists(path):
             self.logger.info('Model - Path [{}] does not exist. Creating...'
                              ''.format(path))
             create_dir(path)
-        self.model.save(os.path.join(path, name + '_model.h5'))
-        self.model.save_weights(os.path.join(path, name + '_weights.h5'))
+        self.model.save(os.path.join(path, self.name + '_model.h5'))
+        self.model.save_weights(os.path.join(path, self.name + '_weights.h5'))
 
-    # TODO: LOAD MODEL AND WEIGHTS FROM A GIVEN PATH!!
     def load_model(self, model_path, weights_path):
         """
-        # TODO: ####################################################################
+        It loads the model and the weights from file. It corresponds to a
+        previuosly trained deep learning model
         :param model_path:
         :param weights_path:
         """
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import os
 from analyzer import Analyzer
 from common.utils import load_config, init_logger
 from preprocessor import Preprocessor
+from classifiers.age_classifier import AgeClassifier
 
 if __name__ == '__main__':
     cfg = load_config('./conf/conf.yaml')
@@ -42,93 +43,135 @@ if __name__ == '__main__':
     ##############################################################
     #                        CNN MODEL                           #
     ##############################################################
+    train_path = os.path.join(cfg['data']['sorted_path'], 'training')
+    test_path = os.path.join(cfg['data']['sorted_path'], 'test')
+    fig_full_path = os.path.abspath(cfg['analysis']['figures_path'])
+    model_full_path = os.path.abspath('classifiers/models')
+
+    cnn = AgeClassifier(train_path, test_path, fig_path=fig_full_path)
+    cnn.compile()
+    cnn.plot_model(os.path.join(fig_full_path, 'model_architecture.png'))
+    cnn.train()
+    cnn.show_training_history()
+    cnn.plot_confusion_matrix((4742 // 32), cfg['data']['classes_list'])
+    cnn.save_model(model_full_path)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     # TODO:
-    from tensorflow.keras.preprocessing.image import ImageDataGenerator
-    from tensorflow.keras import Sequential
-    from tensorflow.keras.layers import Conv2D, MaxPool2D, Dropout, Flatten, \
-        Dense
-    import tensorflow as tf
-    physical_devices = tf.config.experimental.list_physical_devices('GPU')
-    tf.config.experimental.set_memory_growth(physical_devices[0], True)
-
-    training_datagen = ImageDataGenerator(rescale=1. / 255,
-                                          rotation_range=0.3,
-                                          shear_range=0.1,
-                                          zoom_range=[0.90, 1.2],
-                                          horizontal_flip=True)
-    test_datagen = ImageDataGenerator(rescale=1. / 255)
-    training_path = '/home/ruben/workspace/tfg/deep-learning-facial-recognition/data/age/training'
-    test_path = '/home/ruben/workspace/tfg/deep-learning-facial-recognition/data/age/test'
-
-    training_set = training_datagen.flow_from_directory(training_path,
-                                                        target_size=(64, 64),
-                                                        batch_size=32,
-                                                        class_mode='categorical',
-                                                        shuffle=True,
-                                                        seed=42)
-
-    test_set = test_datagen.flow_from_directory(test_path,
-                                                target_size=(64, 64),
-                                                batch_size=32,
-                                                class_mode='categorical',
-                                                shuffle=True,
-                                                seed=42)
-    age_classifier = Sequential()
-    age_classifier.add(
-        Conv2D(filters=32, kernel_size=(3, 3), input_shape=(64, 64, 3),
-               activation='relu'))
-    age_classifier.add(MaxPool2D(pool_size=(2, 2)))
-    age_classifier.add(
-        Conv2D(filters=32, kernel_size=(3, 3), activation='relu'))
-    age_classifier.add(MaxPool2D(pool_size=(2, 2)))
-    age_classifier.add(
-        Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
-    age_classifier.add(MaxPool2D(pool_size=(2, 2)))
-    age_classifier.add(Flatten())
-    age_classifier.add(Dense(units=128, activation='relu'))
-    age_classifier.add(Dropout(0.3))
-    age_classifier.add(Dense(units=64, activation='relu'))
-    age_classifier.add(Dropout(0.3))
-    age_classifier.add(Dense(units=6, activation='softmax'))
-    age_classifier.compile(optimizer='adam', loss='categorical_crossentropy',
-                           metrics=['accuracy'])
-    hist = age_classifier.fit(training_set,
-                              steps_per_epoch=(18966 // 32),
-                              epochs=25,
-                              validation_data=test_set,
-                              validation_steps=(4742 // 32))
-
-
-    # TODO: PRINT MODEL SUMMARY (LAYERS / ARCHITECTURE)
-    from tensorflow.python.keras.utils.vis_utils import plot_model
-    print(age_classifier.summary())
-    plot_model(age_classifier, to_file='figures/model_plot.png',
-               show_shapes=True, show_layer_names=True)
-
-
-    # TODO: SAVE HISTORY OF TRAINING PROCESS AND SHOW SOME GRAPHS ON LOSS AND ACCURACY
-    import matplotlib.pyplot as plt
-    fig, axs = plt.subplots(1, 2)
-    fig.set_size_inches(8, 8)
-    title_font = {'family': 'serif', 'color': 'black', 'weight': 'normal',
-                  'size': 18}
-    label_font = {'family': 'serif', 'color': 'black', 'weight': 'normal',
-                  'size': 14}
-    # Accuracy - Train vs. Test
-    axs[0].plot(hist.history['accuracy'], lw=2)
-    axs[0].plot(hist.history['val_accuracy'], lw=2)
-    axs[0].set_title('Accuracy History', fontdict=title_font)
-    axs[0].set_ylabel('Accuracy', fontdict=label_font)
-    axs[0].set_xlabel('Epoch', fontdict=label_font)
-    axs[0].legend(['train', 'test'], loc='upper left')
-    # Loss - Train vs. Test
-    axs[1].plot(hist.history['loss'], lw=2)
-    axs[1].plot(hist.history['val_loss'], lw=2)
-    axs[1].set_title('Loss History', fontdict=title_font)
-    axs[1].set_ylabel('Loss', fontdict=label_font)
-    axs[1].set_xlabel('Epoch', fontdict=label_font)
-    axs[1].legend(['train', 'test'], loc='upper left')
-    fig.show()
+    # from tensorflow.keras.preprocessing.image import ImageDataGenerator
+    # from tensorflow.keras import Sequential
+    # from tensorflow.keras.layers import Conv2D, MaxPool2D, Dropout, Flatten, \
+    #     Dense
+    # import tensorflow as tf
+    # physical_devices = tf.config.experimental.list_physical_devices('GPU')
+    # tf.config.experimental.set_memory_growth(physical_devices[0], True)
+#
+    # training_datagen = ImageDataGenerator(rescale=1. / 255,
+    #                                       rotation_range=0.3,
+    #                                       shear_range=0.1,
+    #                                       zoom_range=[0.90, 1.2],
+    #                                       horizontal_flip=True)
+    # test_datagen = ImageDataGenerator(rescale=1. / 255)
+    # training_path = '/home/ruben/workspace/tfg/deep-learning-facial-recognition/data/age/training'
+    # test_path = '/home/ruben/workspace/tfg/deep-learning-facial-recognition/data/age/test'
+#
+    # training_set = training_datagen.flow_from_directory(training_path,
+    #                                                     target_size=(64, 64),
+    #                                                     batch_size=32,
+    #                                                     class_mode='categorical',
+    #                                                     shuffle=True,
+    #                                                     seed=42)
+#
+    # test_set = test_datagen.flow_from_directory(test_path,
+    #                                             target_size=(64, 64),
+    #                                             batch_size=32,
+    #                                             class_mode='categorical',
+    #                                             shuffle=True,
+    #                                             seed=42)
+    # age_classifier = Sequential()
+    # age_classifier.add(
+    #     Conv2D(filters=32, kernel_size=(3, 3), input_shape=(64, 64, 3),
+    #            activation='relu'))
+    # age_classifier.add(MaxPool2D(pool_size=(2, 2)))
+    # age_classifier.add(
+    #     Conv2D(filters=32, kernel_size=(3, 3), activation='relu'))
+    # age_classifier.add(MaxPool2D(pool_size=(2, 2)))
+    # age_classifier.add(
+    #     Conv2D(filters=64, kernel_size=(3, 3), activation='relu'))
+    # age_classifier.add(MaxPool2D(pool_size=(2, 2)))
+    # age_classifier.add(Flatten())
+    # age_classifier.add(Dense(units=128, activation='relu'))
+    # age_classifier.add(Dropout(0.3))
+    # age_classifier.add(Dense(units=64, activation='relu'))
+    # age_classifier.add(Dropout(0.3))
+    # age_classifier.add(Dense(units=6, activation='softmax'))
+    # age_classifier.compile(optimizer='adam', loss='categorical_crossentropy',
+    #                        metrics=['accuracy'])
+    # hist = age_classifier.fit(training_set,
+    #                           steps_per_epoch=(18966 // 32),
+    #                           epochs=25,
+    #                           validation_data=test_set,
+    #                           validation_steps=(4742 // 32))
+#
+#
+    # # TODO: PRINT MODEL SUMMARY (LAYERS / ARCHITECTURE)
+    # from tensorflow.python.keras.utils.vis_utils import plot_model
+    # print(age_classifier.summary())
+    # plot_model(age_classifier, to_file='figures/model_plot.png',
+    #            show_shapes=True, show_layer_names=True)
+#
+#
+    # # TODO: SAVE HISTORY OF TRAINING PROCESS AND SHOW SOME GRAPHS ON LOSS AND ACCURACY
+    # import matplotlib.pyplot as plt
+    # fig, axs = plt.subplots(1, 2)
+    # fig.set_size_inches(8, 8)
+    # title_font = {'family': 'serif', 'color': 'black', 'weight': 'normal',
+    #               'size': 18}
+    # label_font = {'family': 'serif', 'color': 'black', 'weight': 'normal',
+    #               'size': 14}
+    # # Accuracy - Train vs. Test
+    # axs[0].plot(hist.history['accuracy'], lw=2)
+    # axs[0].plot(hist.history['val_accuracy'], lw=2)
+    # axs[0].set_title('Accuracy History', fontdict=title_font)
+    # axs[0].set_ylabel('Accuracy', fontdict=label_font)
+    # axs[0].set_xlabel('Epoch', fontdict=label_font)
+    # axs[0].legend(['train', 'test'], loc='upper left')
+    # # Loss - Train vs. Test
+    # axs[1].plot(hist.history['loss'], lw=2)
+    # axs[1].plot(hist.history['val_loss'], lw=2)
+    # axs[1].set_title('Loss History', fontdict=title_font)
+    # axs[1].set_ylabel('Loss', fontdict=label_font)
+    # axs[1].set_xlabel('Epoch', fontdict=label_font)
+    # axs[1].legend(['train', 'test'], loc='upper left')
+    # fig.show()
 
 
     # TODO: SAVE MODEL, WEIGHTS AND HISTORY (DICT, with pickle) ON DISK FOR LATER WORKING SESSIONS
